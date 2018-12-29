7f7bb0afb4d6c651a4a251a9c1b31abba1a72904
abhishek_Bhosale <abhishek11521@gmail.com>
2018-12-29 09:19:55 +0530

    Initial commit with project structure

---
+++ src/main/java/com/handzap/scrapperService/ScrapperServiceApplication.java
@@ -0,0 +1,14 @@
+package com.handzap.scrapperService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapperServiceApplication.class, args);
+	}
+
+}
+
